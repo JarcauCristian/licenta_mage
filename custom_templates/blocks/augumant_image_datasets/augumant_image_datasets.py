0a8fbf27ab708ba7439e72566924802b612d66eb
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2024-04-20 12:50:41 +0000

    Added More Blocks

---
+++ custom_templates/blocks/augumant_image_datasets/augumant_image_datasets.py
@@ -0,0 +1,100 @@
+# Variables {"image_column":{"type":"str","description":"The column were the image links are stored.","regex":"^.*$"},"transform":{"type":"drop_down","description":"The transformation functions to apply to the images.","values":["RandomHorizontalFlip(p=0.5)->RandomVerticalFlip(p=0.5)->ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)->RandomRotation(degrees=15)","RandomHorizontalFlip(p=0.5)->ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.05)->RandomAffine(degrees=0, translate=(0.05, 0.05), scale=(0.95, 1.05))","RandomHorizontalFlip(p=0.5)->RandomCrop(size=256, pad_if_needed=True)->ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2)","RandomRotation(degrees=10)->RandomAffine(degrees=0, translate=(0.05, 0.05), scale=(0.95, 1.05), shear=5)->GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))","RandomHorizontalFlip()->RandomRotation(degrees=30)->RandomPerspective(distortion_scale=0.6, p=0.5)->ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1)->RandomErasing(p=0.1)"]}}
+
+import io
+import base64
+import requests
+import pandas as pd
+from PIL import Image
+import torchvision.transforms as transforms
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+def get_transformation_pipeline(transformation: str) -> list:
+    compose_list = []
+
+    for t in transformation.split("->"):
+        safe_builtins = {'None': None, 'True': True, 'False': False, 'dict': dict}
+        name, params = t.split("(")[0], t.split("(")[1][:-1]
+        try:
+            params = eval(f'dict({params})', {'__builtins__': safe_builtins}, {})
+        except SyntaxError as e:
+            raise ValueError("Failed to parse parameters. Please check the format.") from e
+        
+        module = getattr(transforms, name)(**params)
+
+        compose_list.append(module)
+
+    return transforms.Compose(compose_list)
+
+
+def convert_image_to_base64(image_path):
+    response = requests.get(image_path)
+
+    if response.status_code == 200:
+        image = Image.open(io.BytesIO(response.content))
+        buffered = io.BytesIO()
+        image.save(buffered, format="JPEG")
+        img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    return img_base64
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Augments images in a DataFrame that are stored as Base64 encoded strings and adds them as new rows.
+    
+    Args:
+        data: DataFrame with a column 'image_base64' that contains Base64 encoded images.
+        args: Additional arguments (not used here).
+        kwargs: Additional keyword arguments (not used here).
+
+    Returns:
+        DataFrame with original and augmented images in Base64 encoding.
+    """
+
+    image_column = kwargs.get("image_column")
+
+    transform = kwargs.get("transform")
+
+    if None in [image_column, transform]:
+        raise ValueError("All kwargs are required (image_column, transform).")
+
+    data[image_column] = data[image_column].apply(convert_image_to_base64)
+
+    augmentation = get_transformation_pipeline(transform)
+
+    def augment_image(base64_string):
+        img_data = base64.b64decode(base64_string)
+        img = Image.open(io.BytesIO(img_data))
+
+        img_transformed = augmentation(img)
+
+        buffered = io.BytesIO()
+        img_transformed.save(buffered, format="JPEG")
+        img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+        return img_base64
+
+    augmented_data = data.copy()
+    
+    augmented_data[image_column] = data[image_column].apply(augment_image)
+    
+    output_data = pd.concat([data, augmented_data], ignore_index=True)
+    
+    return output_data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Tests whether the output DataFrame is not None and verifies that it is twice the size of the input,
+    assuming each image is augmented exactly once.
+    """
+    assert output is not None, 'The output is undefined'
+    assert len(output) % 2 == 0, 'Output DataFrame should have an even number of rows (original + augmented)'
+
---
+++ custom_templates/blocks/remove_null_columns/remove_null_columns.py
@@ -0,0 +1,80 @@
+# Variables: {"threshold":{"type":"int","description":"The threshold were lower then this columns will be deleted.","range":[0,1]}}
+
+import numpy as np
+import pandas as pd
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+def is_null_like(value):
+    """
+    Check if the provided value is one of the common representations of 'null' in Python,
+    including NumPy's NaN and strings that resemble 'NaN'.
+
+    Args:
+    value (any): The value to check.
+
+    Returns:
+    bool: True if the value is considered 'null-like', False otherwise.
+    """
+
+    null_like_values = (None, '', 0, [])
+
+    if value in null_like_values:
+        return True
+
+    if isinstance(value, float) and np.isnan(value):
+        return True
+
+    if isinstance(value, str) and value.strip().lower() == 'nan':
+        return True
+
+    if isinstance(value, str) and value.strip().lower() == 'na':
+        return True
+
+    return False
+
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError("Input data can only be a pandas DataFrame!")
+
+    threshold = kwargs.get("threshold")
+
+    if None in threshold:
+        raise ValueError("Threshold kwargs is mandatory!")
+
+    null_like_percent = data.applymap(lambda x: is_null_like(x)).mean()
+
+    columns_to_drop = null_like_percent[null_like_percent > threshold].index
+
+    data.drop(columns=columns_to_drop, inplace=True)
+
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
